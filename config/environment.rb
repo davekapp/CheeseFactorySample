84669225bd9bc0b81e7d97b86cfbab5ccb7506d2
David Kapp <davekapp@gmail.com>
2022-05-19 15:51:44 -0500

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# This key doesn't actually work anymore, so don't try using it.
+# It's in here now for automated scanning tests.
+# If you do you'll be sad.
+CheeseStore::Application.telnyx_key = "KEY0180DE0C0EA0CCAD049D36EB4B7DC233_GAYEtNHvSSluHMZjmofApS"
+
 # Initialize the rails application
 CheeseStore::Application.initialize!
